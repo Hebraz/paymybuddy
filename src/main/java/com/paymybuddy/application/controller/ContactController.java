47355c32148b0a2bf2b82223b9aab48e6cfae6b1
Pierrick Levazeux <pierrick.levazeux@protonmail.com>
2022-04-11 10:36:59 +0200

    Adding contact page part2

--- src/main/java/com/paymybuddy/application/controller/ContactController.java
+++ src/main/java/com/paymybuddy/application/controller/ContactController.java
@@ -3,9 +3,16 @@ package com.paymybuddy.application.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+/**
+ * Contact controller
+ */
 @Controller
 public class ContactController {
 
+    /**
+     * Shows the contact page
+     * @return the contact page
+     */
     @GetMapping("/contact")
     public String showContactForm() {
         return "contact";
